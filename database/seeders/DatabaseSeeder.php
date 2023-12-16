ad1b73bf6db3de3a61e5c7e0a8210120ef01ac86
rahmares <rr0145551@gmail.com>
2023-12-16 08:05:56 +0700

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-        UserSeeder::class,
+        PustakawanSeeder::class,
+        BookShelfSeeder::class,
 ]);
         // \App\Models\User::factory(10)->create();
 
